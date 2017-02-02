f3a463237be354835a55bd6567cfb2f5d947a352
Raphael Medaer <raphael@medaer.me>
2017-02-02 11:29:57 +0100

    Replaced if(context/command) by ContextSwitcher.
    Instead of writing (a lot of) conditions to switch contexts and
    commands in CLI, I implemented a ContextSwitcher which is using
    decorators to select handler.

--- pyolite2/cli.py
+++ pyolite2/cli.py
@@ -4,19 +4,16 @@ import os
 import sys
 import click
 from . import __version__
-from . import Pyolite
+from .pyolite import Pyolite
+from .switcher import ContextSwitcher
 
 def version_msg():
     """ Returns the Pyolite2 version, location and Python powering it. """
-    # Usage of click is inspired from cookicutter project
+    # [NOTE] Usage of click is inspired from cookicutter project
     python_version = sys.version[:3]
     location = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     message = u'Pyolite2 %(version)s from {} (Python {})'
     return message.format(location, python_version)
-
-def list_repos(pyolite):
-    for repo in pyolite.repos:
-        print repo.name
 
 # Defining command and options
 @click.command(context_settings=dict(help_option_names=[u'-h', u'--help']))
@@ -33,9 +30,18 @@ def main(context, command, config):
     pyolite = Pyolite(config)
     pyolite.load()
 
-    if context == 'repo':
-        if command == 'list':
-            list_repos(pyolite)
+    # Instance switcher to select context/command
+    switcher = ContextSwitcher()
+
+    # Define contexts and commands
+    @switcher.command('repo', 'list')
+    def list_repositories():
+        """ List Git repositories. """
+        for repo in pyolite.repos:
+            print repo.name
+
+    # Let's switch !
+    switcher.switch(context, command)
 
 if __name__ == "__main__":
     main()
