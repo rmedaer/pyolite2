a44baffe51ec8df59b33c1e75270ad02bacc1eb6
Raphael Medaer <raphael@medaer.me>
2017-03-09 15:10:50 +0100

    Improved project documentation and setup.

---
+++ HISTORY.rst
@@ -0,0 +1,15 @@
+.. :changelog:
+
+===============
+Release History
+===============
+
+0.2.0 (2017-XX-XX)
+------------------
+
+* Replaced custom command switcher in CLI by click .
+
+0.1.0 (2017-03-09)
+------------------
+
+* First implementation of Pyolite2
--- setup.py
+++ setup.py
@@ -1,22 +1,59 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
-with open('README.md') as f:
-    readme = f.read()
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
 
-with open('LICENSE') as f:
-    license = f.read()
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [
+    'Click>=6.0',
+    # TODO: put package requirements here
+]
+
+test_requirements = [
+    # TODO: put package test requirements here
+]
 
 setup(
     name='pyolite2',
-    version='0.1.0',
-    description='Gitolite admin repository manager.',
-    long_description=readme,
-    author='Raphael Medaer (Escaux)',
-    author_email='rme@escaux.com',
-    url='',
-    license=license,
-    packages=find_packages(exclude=('tests', 'docs'))
+    version='0.3.0',
+    description="Gitolite admin repository manager.",
+    long_description=readme + '\n\n' + history,
+    author="Raphael Medaer",
+    author_email='raphael@medaer.me',
+    url='https://github.com/rmedaer/pyolite2',
+    packages=[
+        'pyolite2',
+    ],
+    package_dir={'pyolite2':
+                 'pyolite2'},
+    entry_points={
+        'console_scripts': [
+            'pyolite2=pyolite2.cli:main'
+        ]
+    },
+    include_package_data=True,
+    install_requires=requirements,
+    license="Apache Software License 2.0",
+    zip_safe=False,
+    keywords='pyolite2',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements
 )
-
